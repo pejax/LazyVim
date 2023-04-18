44ad8e2d51d79435362548c444eead66b6228612
Peter Jacobsen <post@peter-jacobsen.de>
2023-04-18 13:49:55 +0200

    Add trim trailing whitespace

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+  " Disable EditorConfig from removing trailing spaces
+  let g:EditorConfig_disable_rules = ['trim_trailing_whitespace']
+
+  " Trim trailing whitespace for non-markdown files on write
+  function! TrimWhitespace()
+    if (&filetype != 'markdown') && (@% !~# 'neomutt')
+      let l:save = winsaveview()
+      %s/\s\+$//e
+      call winrestview(l:save)
+    endif
+  endfunction
+
+  autocmd BufWritePre * :call TrimWhitespace()
+]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.backupdir = os.getenv("RAMDISK") -- https://neovim.io/doc/user/options.html#%27backupdir%27
+opt.cursorcolumn = true -- https://neovim.io/doc/user/options.html#%27cursorline%27
+opt.directory = os.getenv("RAMDISK") -- https://neovim.io/doc/user/options.html#%27directory%27
+opt.swapfile = false -- https://neovim.io/doc/user/options.html#%27swapfile%27
+opt.undofile = false
