172affc96ad984b95f25920862da4f5a3445d762
Peter Jacobsen <post@peter-jacobsen.de>
2023-04-19 12:07:14 +0200

    Cleanup code

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -69,7 +69,6 @@ if true then return {
           new_section("Grep text",    "Telescope live_grep",  "Telescope"),
           new_section("New file",     "ene | startinsert",    "Built-in"),
           new_section("Quit",         "qa",                   "Built-in"),
-          new_section("Session restore", [[lua require("persistence").load()]], "Session"),
         },
         content_hooks = {
           starter.gen_hook.adding_bullet(pad .. "░ ", false),
@@ -77,32 +76,6 @@ if true then return {
         },
       }
       return config
-    end,
-    config = function(_, config)
-      -- close Lazy and re-open when starter is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "MiniStarterOpened",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      local starter = require("mini.starter")
-      starter.setup(config)
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          local pad_footer = string.rep(" ", 8)
-          starter.config.footer = pad_footer .. "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-          pcall(starter.refresh)
-        end,
-      })
     end,
   },
 
