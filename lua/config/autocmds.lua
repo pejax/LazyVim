8c546b877686156eca15fed8dab22411157c58f6
Peter Jacobsen <post@peter-jacobsen.de>
2024-08-07 12:45:45 +0200

    Add wsv filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,6 +11,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+  pattern = {"*.wsv"},
+  command = "set filetype=wsv"
+})
+
 vim.cmd([[
 
   function! ReplaceTrailingWhitespace()
