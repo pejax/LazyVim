7e625abd2b60a4e6ae805917f4279aa09c1f07ae
Peter Jacobsen <post@peter-jacobsen.de>
2024-08-07 10:57:16 +0200

    Cleanup default options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,20 +15,15 @@
 local opt = vim.opt
 local ramdisk = os.getenv("RAMDISK")
 
-opt.autoindent    = true                  -- https://neovim.io/doc/user/options.html#%27autoindent%27
 opt.autowrite     = false                 -- https://neovim.io/doc/user/options.html#%27autowrite%27
 opt.backup        = false                 -- https://neovim.io/doc/user/options.html#%27backup%27
 opt.backupdir     = ramdisk               -- https://neovim.io/doc/user/options.html#%27backupdir%27
 opt.colorcolumn   = "132"                 -- https://neovim.io/doc/user/options.html#%27colorcolumn%27
 opt.cursorcolumn  = true                  -- https://neovim.io/doc/user/options.html#%27cursorline%27
 opt.directory     = ramdisk               -- https://neovim.io/doc/user/options.html#%27directory%27
-opt.errorbells    = false                 -- https://neovim.io/doc/user/options.html#%27errorbells%27
 opt.laststatus    = 3                     -- https://neovim.io/doc/user/options.html#%27laststatus%27
-opt.showmatch     = true                  -- https://neovim.io/doc/user/options.html#%27showmatch%27
-opt.softtabstop   = 2                     -- https://neovim.io/doc/user/options.html#%27softtabstop%27
 opt.swapfile      = false                 -- https://neovim.io/doc/user/options.html#%27swapfile%27
 opt.timeoutlen    = 500                   -- https://neovim.io/doc/user/options.html#%27timeoutlen%27
-opt.updatetime    = 250                   -- https://neovim.io/doc/user/options.html#%27updatetime%27
 opt.writebackup   = false                 -- https://neovim.io/doc/user/options.html#%27writebackup%27
 if vim.fn.has("mac") then
   opt.guifont = "FiraMono Nerd Font Mono,MesloLGS NF:h10"   --https://neovim.io/doc/user/options.html#%27guifont%27
@@ -39,8 +34,7 @@ end
 -- Globals
 vim.g.maplocalleader = ","
 vim.g.autoformat = false
-vim.g.markdown_recommended_style = 0      -- Fix markdown indentation settings
-vim.g.autocomment = "off"                 -- see autocmd for formatoptions-=acro
+vim.g.autocomment = "off"
 
 vim.cmd([[
   hi CursorLine cterm=underline             " https://neovim.io/doc/user/usr_06.html
