6876a044f10c721fd1529f8e7721fc060b572a55
Peter Jacobsen <post@peter-jacobsen.de>
2024-12-09 00:23:29 +0100

    Re-order dashboard entries

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -4,24 +4,27 @@ return {
     dashboard = {
       preset = {
         keys = {
+          -- Dotfiles
           { icon = " ", key = "a", desc = ".aliases", action = ":e $HOME/.aliases" },
           { icon = " ", key = "e", desc = ".environment", action = ":e $HOME/.environment" },
           { icon = " ", key = "E", desc = ".editorconfig", action = ":e $HOME/.editorconfig" },
           { icon = " ", key = "f", desc = ".functions", action = ":e $HOME/.functions" },
           { icon = " ", key = "n", desc = ".navigation", action = ":e $HOME/.navigation" },
-
-          { icon = " ", key = "F", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          -- LazyVim configuration
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           {
             icon = " ",
             key = "c",
             desc = "Config",
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
+          -- Search
+          { icon = " ", key = "F", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          -- Other
+          { icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
         -- Used by the `header` section
