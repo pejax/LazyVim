6777fa8ba20ad74b94f6f34beb262109864b98a2
Peter Jacobsen <post@peter-jacobsen.de>
2024-12-19 10:58:21 +0100

    Tweak dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -7,8 +7,6 @@ return {
           -- Dotfiles
           { icon = " ", key = "a", desc = ".aliases", action = ":e $HOME/.aliases" },
           { icon = " ", key = "e", desc = ".environment", action = ":e $HOME/.environment" },
-          { icon = " ", key = "E", desc = ".editorconfig", action = ":e $HOME/.editorconfig" },
-          --{ icon = " ", key = "f", desc = ".functions", action = ":e $HOME/.functions" },
           { icon = " ", key = "n", desc = ".navigation", action = ":e $HOME/.navigation" },
           -- LazyVim configuration
           { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
@@ -19,10 +17,10 @@ return {
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
           -- Search
-          { icon = " ", key = "F", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           -- Other
-          { icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
+          --{ icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
