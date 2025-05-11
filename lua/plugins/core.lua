14d8262c31beca66799e5dc8500251436d025bb5
Peter Jacobsen <post@peter-jacobsen.de>
2025-05-11 13:45:57 +0200

    Pin mason-lspconfig to version 1.0 for now as a workaround

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -46,6 +46,9 @@ return {
     },
   },
 
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+
   -- Format diagnostic message to display shellcheck error code [SCxxxx]
   {
     "neovim/nvim-lspconfig",
